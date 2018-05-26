cb8d06f85cb781100de123ee84a35aef1f4fb8bb
maybe4net <maybe_net@inbox.ru>
2018-05-26 11:12:32 +0300

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,30 @@
+"""Blog1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib.auth.views import login
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # Страница регистрации пользователя
+    path('register/', views.register_view, name='register'),
+    # Страница авторизации пользователя
+    path('login/', login, {'template_name': 'users/login.html'}, name='login'),
+    # Страницы завершения сеанса
+    path('logout/', views.logout_view, name='logout'),
+]
